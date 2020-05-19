c9ec605456b076c3e3e98f8911f5f941fb96e9b3
Welington Miyazato <welingtonmiyazato@gmail.com>
2020-05-19 14:18:33 +0900

    Laravel7 - Formatting subscribers count

--- resources/js/components/subscribe-button.js
+++ resources/js/components/subscribe-button.js
@@ -1,3 +1,5 @@
+import numeral from 'numeral'
+
 Vue.component('subscribe-button', {
 	props: {
 		channel: {
@@ -23,6 +25,9 @@ Vue.component('subscribe-button', {
 				return true
 			}
 			return false
+		},
+		count() {
+			return numeral(this.subscriptions.length).format('0a')
 		}
 	},
 	methods: {
