a8cef756f2e7938cbf2d20ce318a6d19888e5ff4
Welington Miyazato <welingtonmiyazato@gmail.com>
2020-05-20 12:31:10 +0900

    Laravel7 - Video conversion and processing with queue jobs

--- app/Jobs/Videos/ConverForStreaming.php
+++ app/Jobs/Videos/ConverForStreaming.php
@@ -1,18 +1,24 @@
 <?php
-
 namespace App\Jobs\Videos;
 
 use App\Video;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Pbmedia\LaravelFFMpeg\FFMpegFacade as FFMpeg;
 
+// use FFMpeg;
 class ConverForStreaming implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    /**
+     *
+     * @var App\Video $video
+     */
     public $video;
 
     /**
@@ -32,6 +38,14 @@ class ConverForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        echo "Converted";
+        $low = (new X264('aac'))->setKiloBitrate(100);
+        $mid = (new X264('aac'))->setKiloBitrate(250);
+        $high = (new X264('aac'))->setKiloBitrate(500);
+        \FFMpeg::fromDisk('local')->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low)
+            ->addFormat($mid)
+            ->addFormat($high)
+            ->save("public/videos/{$this->video->id}/{$this->video->id}.m3u8");
     }
 }
