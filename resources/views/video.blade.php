ff3a980d91ec50fc036ee1eac345982b3176f70b
Welington Miyazato <welingtonmiyazato@gmail.com>
2020-05-21 19:25:20 +0900

    Laravel7 - Video updates

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -5,6 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                @if($video->editable())
+                    <form action="{{ route('videos.update', $video->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                @endif
                 <div class="card-header">{{ $video->title }}</div>
 
                 <div class="card-body">
@@ -15,9 +20,13 @@
                     <div class="d-flex justify-content-between align-items-center">
                         <div>
                             <h4 class="mt-3">
-                                {{ $video->title }}
+                                @if($video->editable())
+                                    <input type="text" name="title" class="form-control" value="{{ $video->title }}">
+                                @else 
+                                    {{ $video->title }}
+                                @endif
                             </h4>
-                            {{ $video->views }} {{ str_plural('view', $video->views) }}
+                            {{ $video->views }} views
                         </div>
 
                         <div>
@@ -81,6 +90,20 @@
 
                     <hr>
 
+                    <div>
+                        @if($video->editable())
+                            <textarea name="description" cols="3" rows="4" class="form-control">{{ $video->description }}</textarea>
+
+                            <div class="text-right mt-3">
+                                <button class="btn btn-info btn-sm" type="submit">Update video details</button>
+                            </div>
+                        @else
+                            {{ $video->description }}
+                        @endif
+                    </div>
+
+                    <hr>
+
                     <div class="d-flex justify-content-between align-items-center mt-5">
                         <div class="media">
                             <img class="rounded-circle" src="https://picsum.photos/id/42/200/200" width="50" height="50" class="mr-3" alt="...">
@@ -95,6 +118,9 @@
                         <subscribe-button :channel="{{ $video->channel }}" :initial-subscriptions="{{ $video->channel->subscriptions }}" />
                     </div>
                 </div>
+                @if($video->editable())
+                </form>
+                @endif
             </div>
         </div>
     </div>
